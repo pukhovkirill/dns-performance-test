d9c3fc02fe942f204a0c44b6bbe3a25b2b207f9d
Pukhov Kirill <kirillpuhov526@gmail.com>
2026-06-23 12:05:44 +0300

    feat(libdns-tests): add test runner entry point

---
+++ libdns/tests/main_test.c
@@ -0,0 +1,13 @@
+// main_test.c
+#include <stdio.h>
+
+// void test_dns_packet_all(void);
+
+int main(void) {
+    printf("Running all tests...\n");
+
+    // test_dns_packet_all();
+
+    printf("All tests passed!\n");
+    return 0;
+}
